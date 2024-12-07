0cf13fa9e2a3bf2c117437c797a52a97da9bb395
fatima <fatima.altamirano@davinci.edu.ar>
2024-12-06 22:36:28 -0300

    trabajando para que el usuario pueda ver los eventos astronomicos de la fecha elegida en el datepicker

--- Components/EventOfADay.js
+++ Components/EventOfADay.js
@@ -1,21 +1,104 @@
 //muestra la imagen del dia elegido
-import { useState } from "react"
-import { Text, View, StyleSheet, Button } from "react-native"
+import { useState, useEffect } from "react"
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity, Image } from "react-native";
+import { imageOfADay } from "../lib/http";
 
-export default function EventOfADay({selected, navigation}){
 
-    const [date, setDate] =useState(selected);
+export default function EventOfADay({day,month,year,setVisible,navigation}){
+
+    const [date, setDate] =useState(`${year}`+'-'+`${month}`+'-'+`${day}`);
+    const [apod, setApod] = useState([]);
+
+    const firstApod = async()=>{
+        try {
+            let newImage = await imageOfADay(date)
+            setApod(newImage);
+            console.log(newImage);
+        } catch (error) {
+            console.log(error)
+        }
+       
+    }
+
+    useEffect(() => {
+     firstApod();
+       }, []);
+     
 
     const showDate=()=>{
-        console.log('date desde eventOfADay '+date)
+        console.log('date desde eventOfADay '+`${day}`+'/'+`${month}`+'/'+`${year}`)
+        //setVisible(false);
     }
 
     return(
-        <View>
-            <Text >HOLA</Text>
-            <Text>fecha ELEGIDA: {date} </Text>
-            <Button title="holis" onPress={()=>{console.log(date)}}/>
-            <Button title="volver" onPress={()=>navigation.goBack()}/>
-        </View>
+        <ScrollView style={styles.card}>    
+            <View style={styles.view}>
+                <Image source={{ uri: apod.hdurl }} style={styles.image} />
+                <Text style={styles.title}>{apod.title}</Text>
+                <Text style={styles.date}>{apod.date}</Text>
+                <Text style={styles.description}>{apod.explanation}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={()=>navigation.navigate('Init')} style={styles.button}>
+                    <Text style={styles.buttonText}>Volver</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+       
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+      backgroundColor: 'pink',
+    },
+    view:{
+      marginTop: 40,
+      alignItems: 'center'
+    },
+    image: {
+      margin:20,
+      padding:150,
+      width: '100%',
+      height: '10%',
+      borderRadius: 10,
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "#fff",
+      marginTop: 10,
+    },
+    description: {
+      fontSize: 16,
+      color: "#eee",
+      textAlign: 'center',
+      margin: 20,
+    },
+    date: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "white",
+      marginBottom: 10,
+    },
+    buttonContainer:{
+      flexDirection: 'row',
+      width: '100%',
+      borderWidth: 2,
+      borderColor: 'white',
+      borderRadius: 6,
+      backgroundColor:'',
+      marginTop: 40,
+      marginBottom:40
+  },
+  button:{
+      flex: 1,
+      alignItems: 'center',
+      padding:16,
+      borderRadius: 6,
+  },
+  buttonText:{
+      fontWeight: "bold",
+      color: 'white'
+  }
+  });
--- Components/MyDatePicker.js
+++ Components/MyDatePicker.js
@@ -5,38 +5,42 @@ import EventOfADay from './EventOfADay';
 
 
 
+
 export default function MyDatePicker({navigation}){
 
    const date = new Date();
     //const [mode, setMode] = useState('date');
+    const [visible, setVisible] = useState(false);
+    const [day, setDay] =useState(date.getDate());
+    const [month, setMonth] =useState(date.getMonth()+1);
+    const [year, setYear] =useState(date.getFullYear());
 
     const pickDay= (e, selectedDate)=>{
-        let day = selectedDate.getDate();
-        let month = selectedDate.getMonth()+1;
-        let year = selectedDate.getFullYear();
+        setDay(selectedDate.getDate());
+        setYear(selectedDate.getFullYear());
         if(month<10){
+            setMonth(`${0}`+`${selectedDate.getMonth()}`);
             console.log('fecha selecccionada: '+`${day}`+'/0'+`${month}`+'/'+`${year}`);
         }else{
+            setMonth(selectedDate.getMonth()+1);
             console.log('fecha selecccionada: '+`${day}`+'/'+`${month}`+'/'+`${year}`);
         }
         //setDate(selectedDate);
-        navigation.navigate('EventOfADay');
-
-        return(
-            <EventOfADay selected={selectedDate}/>  
-        ) 
+        setVisible(true);
     }
       
 
-
     return(
+       
         <ScrollView>
             <View>
                 <Text>Elige una fecha y descubre el evento astronomico de ese dia</Text>
-                    <DateTimePicker value={date} onChange={pickDay} />
-                   
+                   { !visible && <DateTimePicker value={date} onChange={pickDay} />}
+                    {visible && <EventOfADay day={day} month={month} year={year} visible={setVisible}/>}
             </View>
         </ScrollView>
+       
+            
     )
 
 } 
